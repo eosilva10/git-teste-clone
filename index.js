d89d126c9691df819b2c892643a3c49ffc58aca0
Elton Silva <elton_5002@hotmail.com>
2022-05-27 21:06:28 -0300

    Alteração Index

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).send({message: 'ESSE É UM REPOSITÓRIO CLONADO'})
-})
+});
+
+app.get('/ola', (req, res) => {
+    res.status(200).send({message: 'SOU A ROTA OLÁ!'})
+});
 
 app.listen(4001, () => {
     console.log('-- APP INICIANDO NA PORTA 4001 --')
